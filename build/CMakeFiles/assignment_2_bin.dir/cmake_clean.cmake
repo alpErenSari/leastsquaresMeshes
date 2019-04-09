8c713001fca8b04c3409097a7122f2684cb592e7
alpErenSari <sari.eren@metu.edu.tr>
2019-04-09 13:06:34 +0300

    circle mapping code added

---
+++ build/CMakeFiles/assignment_2_bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment_2_bin.dir/main.cpp.o"
+  "assignment_2_bin.pdb"
+  "assignment_2_bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment_2_bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -91,6 +91,48 @@ double tan_over_2_vectors(Eigen::Vector3d &vec_1, Eigen::Vector3d &vec_2)
 
 }
 
+void boundry_map_circle(const Eigen::MatrixXd& V, const Eigen::VectorXi& bnd,
+  Eigen::MatrixXd& bnd_uv)
+{
+  // Get sorted list of boundary vertices
+  std::vector<int> interior,map_ij;
+  map_ij.resize(V.rows());
+
+  std::vector<bool> isOnBnd(V.rows(),false);
+  for (int i = 0; i < bnd.size(); i++)
+  {
+    isOnBnd[bnd[i]] = true;
+    map_ij[bnd[i]] = i;
+  }
+
+  for (int i = 0; i < (int)isOnBnd.size(); i++)
+  {
+    if (!isOnBnd[i])
+    {
+      map_ij[i] = interior.size();
+      interior.push_back(i);
+    }
+  }
+
+  // Map boundary to unit circle
+  std::vector<double> circle_len(bnd.size());
+  circle_len[0] = 0.;
+
+  for (int i = 1; i < bnd.size(); i++)
+  {
+    circle_len[i] = circle_len[i-1] + (V.row(bnd[i-1]) - V.row(bnd[i])).norm();
+  }
+  double total_circle_len = circle_len[circle_len.size()-1] + (V.row(bnd[0]) - V.row(bnd[bnd.size()-1])).norm();
+
+  bnd_uv.resize(bnd.size(),2);
+  for (int i = 0; i < bnd.size(); i++)
+  {
+    double len_frac = circle_len[i] * 2. * igl::PI / total_circle_len;
+    bnd_uv.row(map_ij[bnd[i]]) << cos(len_frac), sin(len_frac);
+  }
+
+}
+
 void parametrization_uniform(Eigen::MatrixXd &V, Eigen::VectorXi &bnd,
   Eigen::MatrixXd &bnd_uv, std::vector<std::vector<double>> &A,
   Eigen::MatrixXd &V_uv_uni)
@@ -274,7 +316,8 @@ int main(int argc, char *argv[])
 
   // Map the boundary to a circle, preserving edge proportions
   Eigen::MatrixXd bnd_uv;
-  igl::map_vertices_to_circle(V,bnd,bnd_uv);
+  // igl::map_vertices_to_circle(V,bnd,bnd_uv);
+  boundry_map_circle(V,bnd,bnd_uv);
 
   std::cout << "bnd_uv size is " << bnd_uv.rows() << '\n';
 
